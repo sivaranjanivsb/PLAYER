9c9d7cbc4e508974fe71eb655539489844970495
sivaranjanivsb <31504326+sivaranjanivsb@users.noreply.github.com>
2018-02-04 11:40:21 +0530

    Update GIVEN ARRAY IS SORTED OR NOT.C

--- GIVEN ARRAY IS SORTED OR NOT.C
+++ GIVEN ARRAY IS SORTED OR NOT.C
@@ -1,22 +1,23 @@
 #include <stdio.h>
 
 int main(void) {
-	int a[5],i,j,c=1,n=1;
+	int a[5],i,j,n=0;
 	for(i=1;i<=5;i++)
 	{
 	scanf("%d",&a[i]);
 	}
-	for(i=0;i<=5;i++)
+	for(i=1;i<=5;i++)
 	{
 		for(j=i+1;j<=5;j++)
 		{
-			if(a[i]<a[j])
+			if(a[j]<a[i])
 			{
 				n++;
+				break;
 			}
 		}
 	}
-	if(n==10)
+	if(n==0)
 	{
 	printf(" sorted");
 	}
