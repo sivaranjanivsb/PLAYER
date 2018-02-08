4c72f70377714df81c5be9188d9bde92022433fe
sivaranjanivsb <31504326+sivaranjanivsb@users.noreply.github.com>
2018-02-08 14:18:35 +0530

    Create set8-min difference in array.c

---
+++ set8-min difference in array.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+
+int main(void) {
+	int a[5],i,j,temp;
+	for(i=1;i<=5;i++)
+	{
+		scanf("%d",&a[i]);
+	}
+	for(i=1;i<=5;i++)
+      { 
+	    for(j=1+i;j<=5;j++)
+	    {
+	    	if(a[i]>a[j])
+	    	{
+	    		temp=a[i];
+	    		a[i]=a[j];
+	    		a[j]=temp;
+	    	}
+	    }
+   
+      }
+   
+    printf("%d\t %d",a[1],a[2]);
+   printf("\t%d",(a[2]-a[1]));
+	return 0;
+}
