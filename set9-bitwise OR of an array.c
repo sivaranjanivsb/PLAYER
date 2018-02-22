9483eb350ac9397b53a117d9464625bc3b61980c
sivaranjanivsb <31504326+sivaranjanivsb@users.noreply.github.com>
2018-02-22 16:11:13 +0530

    Create set9-bitwise OR of an array.c

---
+++ set9-bitwise OR of an array.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int main(void) {
+	int a[10],i,ba=275;
+	for(i=1;i<=5;i++)
+	{
+		scanf("%d",&a[i]);
+	}
+		ba=ba&a[1];
+	for(i=1;i<=5;i++)
+	{
+		ba=ba|a[i];
+	}
+	printf("%d",ba);
+	return 0;
+}
