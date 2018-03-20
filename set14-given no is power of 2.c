38af5f92d3b22762d24c3bebcab018caae75053b
sivaranjanivsb <31504326+sivaranjanivsb@users.noreply.github.com>
2018-03-20 11:28:53 +0000

    Create set14-given no is power of 2.c

---
+++ set14-given no is power of 2.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include<math.h>
+int main(void) {
+          int no,i;
+          scanf("%d",&no);
+          for(i=0;i<no;i++)
+          {
+          if(pow(2,i)==no){
+          printf("yes");}
+          }
+	return 0;
+}
